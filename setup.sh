8ab98932fead96b1e4066e380fa42429f8b8e0d8
keith-ferney <keith@ferney.org>
2017-12-07 11:52:18 -0700

    setup fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 
 mkdir salsa/tmp/db/postgres-data -p
+mkdir salsa/tmp/log -p
 
 cp -n salsa/config/deploy/default/config.yml salsa/config/
 cp -n salsa/config/deploy/default/database.yml salsa/config/
