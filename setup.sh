b34ab467eb50977490c40e1958e595b1c9f4463a
keith-ferney <keith@ferney.org>
2018-02-12 15:39:52 -0700

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#mkdir salsa/tmp/db/postgres-data -p
-#mkdir salsa/tmp/log -p
+mkdir salsa/tmp/db/postgres-data -p
+mkdir salsa/tmp/log -p
 
-#cp -n salsa/config/deploy/default/config.yml salsa/config/
-#cp -n salsa/config/deploy/default/database.yml salsa/config/
+cp -n salsa/config/deploy/default/config.yml salsa/config/
+cp -n salsa/config/deploy/default/database.yml salsa/config/
 
-#sudo docker-compose build
-#sudo docker-compose run salsa rake db:create
-#sudo docker-compose run salsa rake db:migrate
-#sudo docker-compose run salsa rake db:seed
+sudo docker-compose build
+sudo docker-compose run salsa rake db:create
+sudo docker-compose run salsa rake db:migrate
+sudo docker-compose run salsa rake db:seed
 
 bash generate_secrets.sh
