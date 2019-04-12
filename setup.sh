729b615e134230f7f5e63bdd51cffab2e8fe1b92
John Pope <john@pope.tech>
2019-04-12 13:36:42 -0600

    update docker compose commands in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ cp -n salsa/config/deploy/default/config.yml salsa/config/
 cp -n salsa/config/deploy/default/database.yml salsa/config/
 
 docker-compose build
-docker-compose run salsa rake db:create
-docker-compose run salsa rake db:migrate
-docker-compose run salsa rake db:seed
+docker-compose run --rm salsa rake db:create
+docker-compose run --rm salsa rake db:migrate
+docker-compose run --rm salsa rake db:seed
 
 bash salsa/generate_secrets.sh
